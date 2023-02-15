ffe2220fdc91e27ccd8d5271a73921141986d79a
nashugame <34041404+nashugame@users.noreply.github.com>
2023-02-15 20:24:34 +0530

    Create keycloak-scripts.js
    Added the mapper code

---
+++ META-INF/keycloak-scripts.js
@@ -0,0 +1,9 @@
+{
+    "mappers": [
+        {
+            "name": "CIP Email to FirstLast string",
+            "fileName": "cip-email-to-firstlast-string.js",
+            "description": "My Mapper to get unique login id based on email"
+        }
+    ]
+}
